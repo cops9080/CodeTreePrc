92d6e4ae94e01a8862cd6f1806e137c16a6469be
cops9080 <up93py@gmail.com>
2025-07-04 21:49:54 +0900

    문자열, 수 변환

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String a = "Hello World";
+        String Int = "10";
+        String Doub = "2.5";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        int A = Integer.parseInt(Int);
+        Double B = Double.parseDouble(Doub);
+
+        System.out.println(A);
+        System.out.println(B);
     }
 }
