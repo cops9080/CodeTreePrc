c59fb5302beb2374350c3e741e9731f13455b8b8
cops9080 <up93py@gmail.com>
2025-07-04 22:15:57 +0900

    문자열 연습 (인덱싱)

---
+++ src/StringArr/strPrac1.java
@@ -0,0 +1,13 @@
+package StringArr;
+
+public class strPrac1 {
+    public static void main(String[] args) {
+        String text = "banana";
+
+        char ch1 = text.charAt(0);
+        char ch2 = text.charAt(1);
+
+        System.out.println(ch1);
+        System.out.println(ch2);
+    }
+}
